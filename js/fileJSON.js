bb5f2cbb71537714384aa2f3e576e71a0be62e7e
Walter Ordoñez <walter.e.ordonez@gmail.com>
2019-02-21 09:03:54 -0300

    separando el json en un archivo extermo

--- js/fileJSON.js
+++ js/fileJSON.js
@@ -1,19 +1,4 @@
-/*
-//<script type="text/javascript" src="estudiantes.json"></script>
-  var estudiantes = '['+
-      '{"codigo":"001","nombre":"Juan Saavedra","nota":7.52},'+
-      '{"codigo":"002","nombre":"Mariano Perron","nota":8.32},'+
-      '{"codigo":"003","nombre":"Griselda Ordoqui","nota":5.40},'+
-      '{"codigo":"004","nombre":"Juliana Massini","nota":6.50},'+
-      '{"codigo":"005","nombre":"Pedro Lopez","nota":9.25},'+
-      '{"codigo":"006","nombre":"Oscar Robledo","nota":8.25},'+
-      '{"codigo":"007","nombre":"Gregorio Cerrano","nota":4.25},'+
-      '{"codigo":"008","nombre":"Marcela Grimaldi","nota":2.25},'+
-      '{"codigo":"009","nombre":"German Sosa","nota":7.80},'+
-      '{"codigo":"010","nombre":"Ignacio Lezacano","nota":9.10}'+
-  ']';
-  var obJson = JSON.parse(estudiantes);
-*/
+
 (function($){
 
   var data = {
@@ -147,7 +132,7 @@
     if ($('#text-nombre').val() && $('#text-nota').val() )
     {
       estudiantes.push(dato);
-      
+
       $('#text-nombre').val('');
       $('#text-nota').val('');
       $('#text-nombre').focus();
